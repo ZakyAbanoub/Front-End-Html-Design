d9391f334382b426c8a623dbe76c1fdceddfc021
CptPackage <abdelrhmanabdalla15@gmail.com>
2020-06-19 00:50:32 +0200

    Initial Commit

---
+++ js/plugins.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("body").niceScroll();
+});
+$(window).on('load', function() {
+    $('.sk-folding-cube').fadeOut(1700, function(){
+        $('.animate-div').fadeOut(1000);
+    });
+});
+
